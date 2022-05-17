64972901c53da209035a7410d157702b4cc86e04
Kimyeonho <kyh0703@nate.com>
2022-05-17 23:21:31 +0900

    Update kakao-string.py

--- kakao-string.py
+++ kakao-string.py
@@ -1,7 +1,6 @@
 def solution(s):
     answer = 0
     number_dic = {"zero" : 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
-
     pos = 0
     number_list = []
     for i in range(len(s)):
@@ -16,3 +15,20 @@ def solution(s):
     return answer
 
 print(solution("one4seveneight"	))
+
+"""
+def solution(s):
+    words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    for i in range(len(words)):
+        s = s.replace(words[i], str(i))
+
+    return int(s)
+ """
+"""
+def solution(s):
+    answer = s
+    for key, value in num_dic.items():
+        answer = answer.replace(key, value)
+    return int(answer)
+ """
