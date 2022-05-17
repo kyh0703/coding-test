7c7531c618b7b754c150ef6f11c091744ff1313c
Kimyeonho <kyh0703@nate.com>
2022-05-17 23:17:53 +0900

    Update kakao-string.py

---
+++ kakao-string.py
@@ -0,0 +1,18 @@
+def solution(s):
+    answer = 0
+    number_dic = {"zero" : 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+
+    pos = 0
+    number_list = []
+    for i in range(len(s)):
+        number = s[pos:i+1]
+        if number in number_dic:
+            number_list.append(str(number_dic[number]))
+            pos = i + 1
+        if number.isdigit():
+            number_list.append(str(number))
+            pos = i + 1
+    answer = int("".join(number_list))
+    return answer
+
+print(solution("one4seveneight"	))
