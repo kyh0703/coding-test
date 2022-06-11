b55c58e0809b49af9a6f953a6ee03618f7a0f0fa
Kimyeonho <kyh0703@nate.com>
2022-06-11 11:16:00 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,23 @@
-def solution(numbers):
-    answer = []
-    for i in range(len(numbers)):
-        for j in range(i + 1, len(numbers)):
-            answer.append(numbers[i] + numbers[j])
-    answer = list(set(answer))
-    answer.sort()
+def solution(arr, n, m):
+    answer = 0
+    divide = n//m
+    row_sum = dict()
+    for row in range(n):
+        for i in range(divide, n, divide):
+            prev = row_sum.get(row, arr[row][i-i:i])
+            tail = reversed(arr[row][i:i+i])
+            sum = [x + y for x, y in zip(prev, tail)]
+            row_sum[row] = sum
+            if answer < max(sum):
+                answer = max(sum)
     return answer
 
 
-print(solution([2, 1, 3, 4, 1]))
-print(solution([5, 0, 2, 7]))
+print(solution([
+    [5, 1, 8, 2, 3, 3],
+    [6, 5, 2, 1, 3, 3],
+    [1, 1, 5, 6, 3, 3],
+    [1, 1, 1, 1, 3, 3],
+    [1, 1, 1, 1, 3, 3],
+    [1, 1, 1, 1, 3, 3],
+], 6, 3))
