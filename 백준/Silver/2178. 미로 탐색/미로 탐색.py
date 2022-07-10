1e2cacff878e25c1551cbf2d4ac31be2ad674f72
Kimyeonho <kyh0703@nate.com>
2022-07-10 17:40:13 +0900

    [Silver I] Title: 미로 탐색, Time: 104 ms, Memory: 32452 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,32 +1,29 @@
+import sys
 from collections import deque
 
 
-def bfs(x, y):
+dy = (0, 1, 0, -1)
+dx = (1, 0, -1, 0)
+
+def validate(y, x):
+    return (0 <= y < N and 0 <= x < M)
+
+def bfs(y, x):
     q = deque()
-    q.append((x, y))
+    q.append((y, x))
     while q:
-        nx, ny = q.popleft()
-        # 방향을 살핀다
+        qy, qx = q.popleft()
         for i in range(4):
-            xx = nx + dx[i]
-            yy = ny + dy[i]
-            if xx < 0 or xx >= n or yy < 0 or yy >= m:
-                continue
-            if maps[xx][yy] == 0:
-                continue
-            if maps[xx][yy] == 1:
-                maps[xx][yy] = maps[nx][ny] + 1
-                q.append((xx, yy))
+            ny = qy + dy[i]
+            nx = qx + dx[i]
+            if validate(ny, nx) and graph[ny][nx] == 1:
+                graph[ny][nx] = graph[qy][qx] + 1
+                q.append((ny, nx))
+    return graph[N - 1][M - 1]
 
-    return maps[n - 1][m-1]
-
-
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
-
-n, m = map(int, input().split())
-maps = []
-for i in range(n):
-    maps.append(list(map(int, input())))
-
+input = sys.stdin.readline
+N, M = map(int, input().strip().split())
+graph = []
+for _ in range(N):
+    graph.append(list(map(int, input().strip())))
 print(bfs(0, 0))
