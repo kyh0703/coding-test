98a87051f09500057957100f447e5f5d6e419a57
Kimyeonho <kyh0703@nate.com>
2022-07-10 20:44:58 +0900

    [Silver V] Title: 숫자 카드, Time: 1356 ms, Memory: 113492 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,23 @@
+import sys
+from bisect import bisect_left, bisect_right
+
+input = sys.stdin.readline
+N = int(input().strip())
+own_cards = list(map(int, input().strip().split()))
+M = int(input().strip())
+find_cards = list(map(int, input().strip().split()))
+
+
+def is_exist(target):
+    return 1 <= (bisect_right(own_cards, target) - bisect_left(own_cards, target))
+
+answer = []
+own_cards.sort()
+for i in find_cards:
+    if is_exist(i):
+        answer.append(1)
+    else:
+        answer.append(0)
+
+for i in answer:
+    print(i, end=' ')
