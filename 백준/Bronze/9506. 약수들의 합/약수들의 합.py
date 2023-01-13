8530b883743246d172fe185e80f30090b21b3707
Kimyeonho <kyh0703@nate.com>
2023-01-13 15:07:45 +0900

    [Bronze I] Title: 약수들의 합, Time: 60 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.py
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -4,10 +4,6 @@ while True:
     if n == -1:
         break
 
-    if n == 0:
-        print(f'{n} is NOT perfect.')
-        continue
-
     answer = []
     for i in range(1, n):
         if n % i == 0:
