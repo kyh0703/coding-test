bcdf8d8c8fc0064bdd3d1a7b7be5269d4ba6da3b
Kimyeonho <kyh0703@nate.com>
2026-05-24 19:46:36 +0900

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,43 @@
+class MinStack {
+    constructor() {
+        this.stack = []
+        this.minStack = []
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val)
+
+        if (this.minStack.length === 0) {
+            this.minStack.push(val)
+        } else {
+            const min = Math.min(val, this.getMin())
+            this.minStack.push(min)
+        }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop()
+        this.minStack.pop()
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length-1]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minStack[this.minStack.length-1]
+    }
+}
