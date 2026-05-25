3e0fdb85a878d501c579ea4513d6533ea41912ad
Kimyeonho <kyh0703@nate.com>
2026-05-25 10:42:02 +0900

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        s = s.toLocaleLowerCase().replace(/[^a-z0-9]/g, "")
+        let left = 0
+        let right = s.length - 1
+
+        while (left < right) {
+            if (s[left] !== s[right]) {
+                return false
+            }
+
+            left++;
+            right--;
+        }
+
+        return true
+    }
+}
