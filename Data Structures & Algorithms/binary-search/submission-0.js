4a035b4f1e0db41e85e263c522dd39a0d00f5910
Kimyeonho <kyh0703@nate.com>
2026-05-25 18:58:44 +0900

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let left = 0;
+        let right = nums.length - 1;
+
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2);
+
+            if (nums[mid] === target) {
+                return mid
+            }
+
+            if (nums[mid] < target) {
+                left++
+            } else {
+                right--;
+            }
+        }   
+
+        return -1
+    }
+}
